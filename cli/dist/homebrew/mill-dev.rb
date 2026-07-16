af7cb4aacee623d1d3325ac6addd86b7e248a55b
Marius S <39998+winding-lines@users.noreply.github.com>
2026-07-16 06:32:42 -0700

    cli: bump mill-dev.rb template to v0.4.51-rc.3

--- cli/dist/homebrew/mill-dev.rb
+++ cli/dist/homebrew/mill-dev.rb
@@ -3,9 +3,9 @@ class MillDev < Formula
   homepage "https://github.com/millfolio/vault"
   # version / url / sha256 are bumped per release by dist/homebrew/update-formula.sh
   # (downloads the mill-macos.tar.gz release asset and fills in its checksum).
-  version "0.4.51-rc.2"
-  url "https://github.com/millfolio/vault/releases/download/v0.4.51-rc.2/mill-macos.tar.gz"
-  sha256 "d749880862a3034d462b7a13ef23b8092ef573f14457ba4b9bd5a369fa7832c3"
+  version "0.4.51-rc.3"
+  url "https://github.com/millfolio/vault/releases/download/v0.4.51-rc.3/mill-macos.tar.gz"
+  sha256 "b0b4f5cc2c98431f9aee5021141e384142b5372ddb85be75caebf2b21929674c"
 
   depends_on :macos
 
